ac7aab6c0f9dc20ff18a3c40eb8b740abf90cd9a
okdevwww <cuidev713@gmail.com>
2017-07-13 23:00:13 -0700

    profile
    git commit -am profile

--- src/routes/mainUI/profile.js
+++ src/routes/mainUI/profile.js
@@ -34,38 +34,52 @@ class Profile extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: '#0B486B', padding:10 }} >
-          <View style={{  justifyContent: 'center', alignItems: 'center', marginTop: 8 }}>
-            <View style={styles.ball} />
-            <Image source={require('../../../assets/images/profile.png')} style={{ height: 150, width: 150, borderRadius: 75, position: 'absolute' }} />
-          </View>
-          <View style={{ alignItems: 'center', justifyContent: 'flex-start', paddingTop: 2 }}>
-            <Text style={{ fontSize: 26, color: 'white', fontWeight: 'bold', textShadowColor: 'black', textShadowOffset: { width: 2, height: 1 } }}>{this.state.details.name}</Text>
-            <Text style={{ fontSize: 18, color: 'white', paddingBottom: 2 }}>{this.state.details.email}</Text>
-          </View>
-        </View>
-        <View style={{ backgroundColor: 'rgba(17, 109, 161, 0.9)'}}>
-          <View style={{ flexDirection: 'row'}}>
-            <View style={styles.socialView}>
-              <Text style={styles.socialTextValue}>1094</Text>
-              <Text style={styles.socialTextTitle}>FOLLOWERS</Text>
+      <Container>
+        <Content>
+          <View style={{ backgroundColor: '#0B486B', padding: 10 }} >
+            <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 8 }}>
+              <View style={styles.ball} />
+              <Image source={require('../../../assets/images/profile.png')} style={{ height: 150, width: 150, borderRadius: 75, position: 'absolute' }} />
             </View>
-            <View style={styles.socialView}>
-              <Text style={styles.socialTextValue}>223</Text>
-              <Text style={styles.socialTextTitle}>FOLLOWING</Text>
-            </View>
-            <View style={{ flex: 1, justifyContent: 'center', padding:5, alignItems: 'center' }}>
-              <Text style={styles.socialTextValue}>88</Text>
-              <Text style={styles.socialTextTitle}>LIKES</Text>
+            <View style={{ alignItems: 'center', justifyContent: 'flex-start', paddingTop: 2 }}>
+              <Text style={{ fontSize: 26, color: 'white', fontWeight: 'bold', textShadowColor: 'black', textShadowOffset: { width: 2, height: 1 } }}>{this.state.details.name}</Text>
+              <Text style={{ fontSize: 18, color: 'white', paddingBottom: 2 }}>{this.state.details.email}</Text>
             </View>
           </View>
-        </View>
-        <Container>
-          <Content>
-          </Content>
-        </Container>
-      </View>
+          <View style={{ backgroundColor: 'rgba(17, 109, 161, 0.9)' }}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={styles.socialView}>
+                <Text style={styles.socialTextValue}>1094</Text>
+                <Text style={styles.socialTextTitle}>FOLLOWERS</Text>
+              </View>
+              <View style={styles.socialView}>
+                <Text style={styles.socialTextValue}>223</Text>
+                <Text style={styles.socialTextTitle}>FOLLOWING</Text>
+              </View>
+              <View style={{ flex: 1, justifyContent: 'center', padding: 5, alignItems: 'center' }}>
+                <Text style={styles.socialTextValue}>88</Text>
+                <Text style={styles.socialTextTitle}>LIKES</Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ flex:1, backgroundColor: 'rgba(196, 219, 231, 0.9)', padding:12 }}>
+            <View style={{ backgroundColor: 'white', flex: 1,  borderRadius: 8 }}>
+              <View style={{ alignItems: 'center', paddingBottom: 5, borderBottomWidth: 1, borderColor: '#EDF4F7' }}>
+                <Text style={{ fontSize: 16 }}>Verified</Text>
+                <View style={{ flexDirection: 'row' }}>
+                  <Icon name='logo-facebook' style={{ marginRight: 10, color: '#3B5895' }} />
+                  <Icon name='md-mail' />
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                  <Text style={{ fontSize: 16 }}>Member since </Text>
+                  <Text style={{ fontSize: 16 }}>12/06/2017</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+
+        </Content>
+      </Container>
 
     );
   }
@@ -95,7 +109,7 @@ const styles = StyleSheet.create({
 
   socialView: {
     flex: 1, justifyContent: 'center', alignItems: 'center',
-    borderColor: 'rgba(31, 95, 97, 0.9)', borderRightWidth: 1,padding:5
+    borderColor: 'rgba(31, 95, 97, 0.9)', borderRightWidth: 1, padding: 5
   }
 });
 
