98fb907224ae7de695d6f1b4fd79c76423d2a3f3
Bohdan Lou <devstar713@yahoo.com>
2017-07-17 21:32:33 +0200

    remove

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './auth_reducer';
-import regReducer from './reg_reducer';
-
-export default combineReducers({
-  auth: authReducer,
-  reg: regReducer
-});
