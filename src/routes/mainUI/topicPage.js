c3e722df61caf97a7bdc50c77c51d951e508b62e
ok713 <devstar713@yahoo.com>
2017-09-15 03:32:43 -0700

    topic page

--- src/routes/mainUI/topicPage.js
+++ src/routes/mainUI/topicPage.js
@@ -18,9 +18,10 @@ const SCREEN_WIDTH = Dimensions
 const {width, height} = Dimensions.get('window');
 const HORIZONTAL_PADDING = 8;
 const data = [{pic: require('../../../assets/images/profile.png'), name: 'Laura Lee', day: '4d ago', comments: 1, likes:14,
-              topic: 'I am a dedicated person. I enjoy reading, and the knowledge and perspective that my reading gives me has strengthened my teaching skills' },
+              topic: 'I am a dedicated person. I enjoy reading, and the knowledge and perspective that my reading gives me has strengthened my teaching skills asdfsdfsdfsdfsdfsdsfd' },
               {pic: require('../../../assets/images/profile.png'), name: 'John David', day: '4d ago', comments: 1, likes:14,
-              topic: 'I am a dedicated person. I enjoy reading, and the knowledge and perspective that my reading gives me has strengthened my teaching skills' },
+              image: require('../../../assets/images/Announcement_banner.jpg')
+             },
               {pic: require('../../../assets/images/profile.png'), name: 'Eric Lou', day: '4d ago', comments: 1, likes:14,
               topic: 'I am a dedicated person. I enjoy reading, and the knowledge and perspective that my reading gives me has strengthened my teaching skills' },                            
             ]
@@ -67,31 +68,53 @@ class TopicPage extends Component {
 
   renderItem(item, index) {
     return (
-          <View>
+          <View style = {{ borderWidth: 1, borderRadius: 8, borderColor: '#c9c9c9', marginBottom: 13}}>
             <View style = {{flexDirection: "row", alignItems: 'center', padding: 10}}>
-              <Image source = {item.pic} style = {{ width: 50, height: 50, borderRadius: 25 }}/>
+              <Image source = {item.pic} style = {{ width: 55, height: 55, borderRadius: 17 }}/>
               <View style = {{paddingHorizontal: 10}} >
-                <Text style = {{color: '#515151', fontSize:15}} >{item.name}</Text>
-                <Text>{item.day}</Text>
+                <Text style = {styles.nameText} >{item.name}</Text>
+                <Text style ={{ color: '#a4a4a4', fontSize: 15}} >{item.day}</Text>
               </View>
             </View>
-            <View style = {{padding: 10}}>
-              <Text numberOfLines = {this.state.moreList[index] ? null : 2} >{item.topic}</Text>
+            <View style = {{ paddingBottom: 5}}>
+              {
+                item.topic &&
+                <Text style ={{ color: '#ababab', fontSize: 15, paddingHorizontal: 10}} numberOfLines = {this.state.moreList[index] ? null : 3} >{item.topic}</Text>
+              }
+              {
+                item.image &&
+                  <View>
+                    <Text style = {[styles.nameText, {paddingHorizontal: 10}]}>
+                      {item.name}
+                      <Text style = {{ color: '#ababab', fontSize: 15}}> added a new photo </Text>
+                    </Text>
+                    <Image source = {item.image} style ={{ marginTop: 10,  width: '100%', height: 200}}/>
+                  </View>
+              }
             </View>
-            <TouchableOpacity style={{paddingHorizontal: 10, paddingBottom: 10}} onPress={(e)=>this.onclickMore(index)}>
-              <Text style = {styles.commentText} >{ this.state.moreList[index] ? "Less" : "See more"}</Text>
+            <TouchableOpacity style={{paddingHorizontal: 10, paddingBottom: 5}} onPress={(e)=>this.onclickMore(index)}>
+              <Text style = {{ color: '#f69021', fontSize: 16}} >{ this.state.moreList[index] ? "Less" : "See More"}</Text>
             </TouchableOpacity>
-            <TouchableWithoutFeedback onPress = {(e)=>{this.navigate(item)}}>
-              <View style = {{padding:10, flexDirection: 'row', justifyContent:'space-between', alignItems:'center', borderColor: '#ececec', borderBottomWidth: 1, borderTopWidth: 1 }}>
-                <Text style = {styles.commentText} >{item.comments} comment</Text>
-                <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
-                  <Text style = {styles.commentText}>{item.likes}</Text>
-                  <TouchableOpacity onPress={(e)=>this.onclickLike(index)} >
-                    <Icon style={{ fontSize:17, color: '#515151' }} name = {this.state.likeList[index]?'ios-heart':'ios-heart-outline'} />
-                  </TouchableOpacity>
-                </View>
+            <View style = {{ flexDirection: 'row', alignItems:'center', paddingHorizontal: 10, paddingBottom: 5}}>
+              <View style = {{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
+                <TouchableOpacity onPress={(e)=>this.onclickLike(index)} >
+                  <Icon style={{ fontSize:20, marginTop: 3, color: this.state.likeList[index]?'#f69021':'#515151' }} name = {this.state.likeList[index]?'ios-heart':'ios-heart-outline'} />
+                </TouchableOpacity>
+                <Text style = {{ color: '#a4a4a4', paddingLeft: 5}}>{item.likes}</Text>
               </View>
-            </TouchableWithoutFeedback>
+              <View style = {{ marginLeft: 20, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
+                <TouchableOpacity onPress={(e)=>this.navigate(item)} >
+                  <Icon style={{ fontSize:20, marginTop: 3, color: '#515151' }} name = {'ios-chatbubbles-outline'} />
+                </TouchableOpacity>
+                <Text style = {{ color: '#a4a4a4', paddingLeft: 5}}>{item.comments}</Text>
+              </View>
+              <View style = {{ marginLeft: 20, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
+                <TouchableOpacity onPress={(e)=>{}} >
+                  <Icon style={{ fontSize:27, marginTop: 3, color: '#515151' }} name = {'ios-send-outline'} />
+                </TouchableOpacity>
+              </View>
+              
+            </View>
 
           </View>
     );
@@ -99,8 +122,8 @@ class TopicPage extends Component {
 
   render() {
     return (
-      <Container style={{backgroundColor: '#fff'}}>
-        <Content>
+      <Container>
+        <Content padder style = {{backgroundColor: '#fff'}}>
        {
          this.state.data.map((item, index)=>{
            return <View key={index}>
@@ -119,9 +142,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  commentText: {
-    color: '#515151'
-  }
+  nameText: {color: '#656565', fontSize:17}
 });
 
 export default TopicPage;
